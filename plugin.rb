8b2720b4f6f4563f4e8b0eb367c196d91f2051f2
Vipul Sharma <vipulsharma144@gmail.com>
2024-06-15 09:32:55 +0530

    added branch

--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,7 @@
 # version: 0.1
 # author: Robin Ward
 
-gem "a-discourse-omniauth-jwt", "0.0.4",git: "https://github.com/univ-ai/a-discourse-omniauth-jwt", require: false
+gem "a-discourse-omniauth-jwt", "0.0.4",source: "https://github.com/univ-ai/a-discourse-omniauth-jwt", branch:"main", require: false
 require 'omniauth/jwt'
 
 class JWTAuthenticator < Auth::ManagedAuthenticator
